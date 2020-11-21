7dd2e2ef1b47530f372e47e636b585576389f95d
root <root@172.16.16.65>
2020-11-21 15:59:11 +0800

    Signed-off-by: root <root@172.16.16.65>
    update

--- setup.sh
+++ setup.sh
@@ -1,1 +1,4 @@
-kind create cluster --image registry.cn-beijing.aliyuncs.com/k7scn/node:v1.19.1 --kubeconfig ./kind.kubeconfig
+docker pull registry.cn-beijing.aliyuncs.com/k7scn/node:v1.19.1
+kind create cluster --image registry.cn-beijing.aliyuncs.com/k7scn/node:v1.19.1 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+
+kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
