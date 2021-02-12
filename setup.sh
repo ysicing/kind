01c67b8d210f811e09e1cdd971d28c62249e93ad
ysicing <i@ysicing.me>
2021-02-12 18:50:49 +0800

    feat(kind): 1.20.2
    1.20.2
    
    Signed-off-by: ysicing <i@ysicing.me>

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-docker pull registry.cn-beijing.aliyuncs.com/k7scn/node:v1.19.1
-kind create cluster --image registry.cn-beijing.aliyuncs.com/k7scn/node:v1.19.1 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+docker pull registry.cn-beijing.aliyuncs.com/k7scn/node:v1.20.2
+
+kind create cluster --image registry.cn-beijing.aliyuncs.com/k7scn/node:v1.20.2 --kubeconfig ./kind.kubeconfig --config ./config.yaml
 
 kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
