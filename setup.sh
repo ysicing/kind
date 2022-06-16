d636c6da7ca6fa3bbbfecdc46984519cef12d72d
ysicing <i@ysicing.me>
2022-06-16 10:12:59 +0800

    feat(update): update version
    update version
    
    Signed-off-by: ysicing <i@ysicing.me>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-docker pull ccr.ccs.tencentyun.com/k7scn/node:v1.23.4
+docker pull ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6
 
-kind create cluster --image ccr.ccs.tencentyun.com/k7scn/node:v1.23.4 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+kind create cluster --image ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6 --kubeconfig ./kind.kubeconfig --config ./config.yaml
 
 kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
