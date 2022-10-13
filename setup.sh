443c2b9ad4c2bf3a68fcac9b6a60da66996a89ab
ysicing <i@ysicing.me>
2022-10-13 12:49:28 +0800

    feat(update): update version
    update version
    
    Signed-off-by: ysicing <i@ysicing.me>

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CN: ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6
-docker pull kindest/node:v1.23.6
+docker pull kindest/node:v1.23.12
 
-kind create cluster --image kindest/node:v1.23.6 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+kind create cluster --image kindest/node:v1.23.12 --kubeconfig ./kind.kubeconfig --config ./config.yaml
 
 kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
