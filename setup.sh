63811102b4969a944cef3a9b8da6a7c8bc80caae
缘生 <i@ysicing.me>
2023-02-13 14:15:33 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CN: ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6
-docker pull kindest/node:v1.25.3
+docker pull ccr.ccs.tencentyun.com/k7scn/kindnode:v1.25.3
 
-kind create cluster --image kindest/node:v1.25.3 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+kind create cluster --image ccr.ccs.tencentyun.com/k7scn/kindnode:v1.25.3 --kubeconfig ./kind.kubeconfig --config ./config.yaml
 
 kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
