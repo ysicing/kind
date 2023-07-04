494ef2ea062a2ba8d48d117891b889656e40156a
ysicing <i@ysicing.me>
2023-07-04 15:20:10 +0800

    feat(update): update
    update
    
    Signed-off-by: ysicing <i@ysicing.me>

--- setup.sh
+++ setup.sh
@@ -1,5 +1,20 @@
-docker pull ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6
+#!/bin/bash
 
-kind create cluster --image ccr.ccs.tencentyun.com/k7scn/kindnode:v1.23.6 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+command_exists() {
+	command -v "$@" > /dev/null 2>&1
+}
 
-kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
+if command_exists kind; then
+  echo "kind exists"
+else
+  wget https://ghproxy.com/https://github.com/kubernetes-sigs/kind/releases/download/v0.20.0/kind-linux-amd64
+  chmod +x kind-linux-amd64
+  mv kind-linux-amd64 /usr/local/bin/kind
+fi
+
+
+docker pull ccr.ccs.tencentyun.com/k7scn/node:v1.27.3
+
+kind create cluster --image ccr.ccs.tencentyun.com/k7scn/node:v1.27.3 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+
+# kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
