889034e4f7cfc89ae45094f1371eae55f5baaf49
ysicing <i@ysicing.me>
2023-07-04 15:38:03 +0800

    feat(update): updatte
    updatte
    
    Signed-off-by: ysicing <i@ysicing.me>

--- setup.sh
+++ setup.sh
@@ -12,8 +12,20 @@ else
   mv kind-linux-amd64 /usr/local/bin/kind
 fi
 
+if command_exists kubectl; then
+  echo "kubectl exists"
+else
+  wget https://pkg-1308438674.cos.ap-shanghai.myqcloud.com/qucheng/cli/stable/tools/kubectl
+  chmod +x kubectl
+  mv kubectl /usr/local/bin/kubectl
+fi
+
 docker pull ccr.ccs.tencentyun.com/k7scn/node:v1.27.3
 
-kind create cluster --image ccr.ccs.tencentyun.com/k7scn/node:v1.27.3 --kubeconfig ./kind.kubeconfig --config ./config.yaml
+kind create cluster --image ccr.ccs.tencentyun.com/k7scn/node:v1.27.3 --kubeconfig ./kind.kubeconfig # --config ./config.yaml
 
 # kubectl apply -f ./kind-ingress.yaml --kubeconfig ./kind.kubeconfig
+
+kubectl cluster-info --context kind-kind --kubeconfig ./kind.kubeconfig
+
+kubectl get nodes
